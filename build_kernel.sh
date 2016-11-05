ae1c8c15abb9cc01f61780c529079d8f5151be20
DevriesL <therkduan@gmail.com>
2016-11-05 23:53:18 +0800

    build: add local scripts

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH 
+export ARCH=arm64
+
 export SEC_BUILD_OPTION_HW_REVISION=02
 
-mkdir out
+export PATH=/home/devries/gcc-linaro-4.9-2016.02-x86_64_aarch64-linux-gnu/bin:$PATH
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android heroqlte_chnzc_defconfig
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
+export CROSS_COMPILE=aarch64-linux-gnu-
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make heroqlte_chnzc_defconfig
+
+make
+
+make dtbs
+
+tools/dtbTool -o dt.img -s 4096 -p scripts/dtc/ arch/arm64/boot/dts/samsung/
+
+tools/mkbootimg \
+      --kernel arch/arm64/boot/Image \
+      --ramdisk ramdisk.packed \
+      --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1" \
+      --base 0x80000000 \
+      --pagesize 4096 \
+      --dt dt.img \
+      --ramdisk_offset 0x02200000 \
+      --tags_offset 0x02000000 \
+      --output boot.img
+
+tar -cvf boot.tar boot.img
