ec124e7a61f00559c9e96f4230db3433e813506f
DevriesL <therkduan@gmail.com>
2016-11-05 14:42:26 +0800

    Merge G9300ZCU2APH1

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,7 @@ export SEC_BUILD_OPTION_HW_REVISION=02
 
 mkdir out
 
-make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android hero2qlte_chnzc_defconfig
+make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android heroqlte_chnzc_defconfig
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
