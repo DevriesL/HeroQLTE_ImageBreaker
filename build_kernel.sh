0778d487006bad2482f401b0a17fdee16593bb8d
DevriesL <therkduan@gmail.com>
2016-11-06 20:57:33 +0800

    build: Change GCC compiler & Use compressed kernel
    AOSP GCC 4.9 compiler from https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+refs

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,9 +4,9 @@ export ARCH=arm64
 
 export SEC_BUILD_OPTION_HW_REVISION=02
 
-export PATH=/home/devries/gcc-linaro-4.9-2016.02-x86_64_aarch64-linux-gnu/bin:$PATH
+export PATH=/home/devries/aarch64-linux-android-4.9-marshmallow-release/bin:$PATH
 
-export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-android-
 
 make heroqlte_chnzc_defconfig
 
@@ -17,7 +17,7 @@ make dtbs
 tools/dtbTool -o dt.img -s 4096 -p scripts/dtc/ arch/arm64/boot/dts/samsung/
 
 tools/mkbootimg \
-      --kernel arch/arm64/boot/Image \
+      --kernel arch/arm64/boot/Image.gz \
       --ramdisk ramdisk.packed \
       --cmdline "console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1" \
       --base 0x80000000 \
